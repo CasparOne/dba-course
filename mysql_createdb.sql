cbba1b9a1672a44894783165a5a5139335087119
CasparOne <vvolodin.victor@gmail.com>
2018-08-27 16:49:27 +0300

    PostgreSQL DB creation command script

--- mysql_createdb.sql
+++ mysql_createdb.sql
@@ -1,1 +1,21 @@
+DROP DATABASE dba;
 CREATE DATABASE dba;
+
+CREATE TABLE books
+(
+    id SERIAL,
+    title VARCHAR(120),
+    author VARCHAR(200),
+    released INT,
+    price FLOAT UNSIGNED,
+    rating INT
+);
+
+
+CREATE TABLE publishers
+(
+    id SERIAL PRIMARY KEY,
+    publishing VARCHAR(240),
+    city VARCHAR(120)
+);
+
--- pg_createdb.sql
+++ pg_createdb.sql
@@ -1,1 +1,19 @@
+DROP DATABASE dba;
 CREATE DATABASE dba;
+
+CREATE TABLE table_name
+(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(120),
+    author VARCHAR(200),
+    released INT,
+    price FLOAT,
+    rating INT
+);
+
+CREATE TABLE public.publishers
+(
+    id SERIAL PRIMARY KEY,
+    publishing VARCHAR(240),
+    city VARCHAR(120)
+);
